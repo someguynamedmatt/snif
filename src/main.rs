5441199fc635de9983edcb8425e62679bfd081ff
Matt <young.qubit@gmail.com>
2018-02-18 00:03:58 -0800

    widdling it down

--- src/main.rs
+++ src/main.rs
@@ -2,13 +2,15 @@
 //!
 //! Quickly (and cleanly) check the configurations of your network devices
 
+extern crate clap;
+extern crate ipnetwork;
 extern crate pnet_datalink;
 extern crate pnet;
-extern crate ipnetwork;
 
 use std::env;
 use std::io;
 use std::io::prelude::*;
+use clap::{Arg, App, SubCommand};
 use ipnetwork::IpNetwork;
 use pnet_datalink::Channel::Ethernet;
 use pnet::packet::ethernet::EthernetPacket;
@@ -18,11 +20,44 @@ use lib::*;
 
 #[cfg(not(test))]
 fn main() {
+    let matches = App::new("SNIf")
+                      .version("1.0")
+                      .author("Matt <young.qubit@gmail.com>")
+                      .about("Simple Network Interface tool")
+                      .arg(Arg::with_name("d")
+                           .short("d")
+                           .long("device")
+                           .value_name("STRING")
+                           .help("device name"))
+                      .get_matches();
+
+    // Calling .unwrap() is safe here because "INPUT" is required (if "INPUT" wasn't
+    // required we could have used an 'if let' to conditionally get the value)
+    //println!("Using version: {}", matches.value_of("v").unwrap());
+
+    // Vary the output based on how many times the user used the "verbose" flag
+    // (i.e. 'myprog -v -v -v' or 'myprog -vvv' vs 'myprog -v'
+    match matches.value_of("d").unwrap() {
+        "stuff" => println!("STUFFFFF"),
+        _ => println!("Don't be crazy"),
+    }
+
+    /*
     let interfaces = pnet_datalink::interfaces();
     let iface_arg = env::args().nth(1).expect("Network interface name not supplied!");
 
     let interface_match = |iface: &pnet_datalink::NetworkInterface| iface.name == iface_arg;
     let interface = interfaces.into_iter().find(interface_match).unwrap();
+    // You can handle information about subcommands by requesting their matches by name
+    // (as below), requesting just the name used, or both at the same time
+    if let Some(matches) = matches.subcommand_matches("test") {
+        if matches.is_present("debug") {
+            println!("Printing debug info...");
+        } else {
+            println!("Printing normally...");
+        }
+    }
+
 
     io::stdout().write("===================\n".as_bytes());
     io::stdout().write("IP:\n".as_bytes());
@@ -54,4 +89,5 @@ fn main() {
         let packet = EthernetPacket::new(rx.next().expect("ERROR WITH PACKET!!!"));
         lib::handle_packet("wlp4s0", &packet.unwrap());
     }
+    */
 }
